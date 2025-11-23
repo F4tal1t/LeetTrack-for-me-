9f0f644a90728754806d084645132aaf341c4eec
Dibyendu Sahoo <dibyendusahoo03@gmail.com>
2025-11-23 16:47:47 +0530

    Time: 11 ms (80.02%) | Memory: 18.9 MB (36.66%) - LeetSync

--- 875-longest-mountain-in-array/longest-mountain-in-array.py
+++ 875-longest-mountain-in-array/longest-mountain-in-array.py
@@ -1,18 +1,13 @@
 class Solution:
-    def longestMountain(self, A: List[int]) -> int:
-        res = 0
-        
-        for indx in range(1, len(A) - 1):
-            if A[indx - 1] < A[indx] > A[indx + 1]:
-                
-                l = r = indx
-                
-                while l > 0 and A[l] > A[l - 1]:
-                    l -= 1
-                
-                while r + 1 < len(A) and A[r] > A[r + 1]:
-                    r += 1
-                
-                res = max(res, (r - l + 1))
-        
-        return res
+    def longestMountain(self, arr: List[int]) -> int:
+        ret = 0 
+        for i in range(1,len(arr)-1):
+            if arr[i-1] < arr[i] > arr[i+1]: # Finds the Peak where both left and right are strictly decreasing
+                l=r=i
+                while l>0 and arr[l]>arr[l-1]: # Moves further down left if decreasing
+                    l-=1
+                while r<len(arr)-1 and arr[r]>arr[r+1]: # Moves Further down right if decreasing
+                    r+=1
+                ret = max(ret,(r-l+1)) # r-l+1 indicates the range of the mountain
+        return ret
+

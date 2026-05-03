781d2dc50480f2a20e51c0ce99365b8400c279cf
Dibyendu Sahoo <dibyendusahoo03@gmail.com>
2026-05-03 18:33:17 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (61.50%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,8 +1,8 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        Pmap = {}
-        for i, n in enumerate(nums):
-            t=target-n
-            if t in Pmap:
-                return [Pmap[t], i]
-            Pmap[n] = i
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        bruh={}
+        for i,n in enumerate(nums):
+            diff= target-n
+            if diff in bruh:
+                return [bruh[diff],i]
+            bruh[n]=i

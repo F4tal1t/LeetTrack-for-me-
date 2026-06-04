58c979a7ba482b56016b3fc61cb375e29904d0d7
Dibyendu Sahoo <dibyendusahoo03@gmail.com>
2026-06-04 10:09:57 +0530

    Time: 1 ms (68.79%) | Memory: 4.7 MB (92.84%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.go
@@ -0,0 +1,23 @@
+func isAnagram(s string, t string) bool {
+    if len(s)!=len(t){
+        return false
+    }
+    chars :=make([]int,26)
+
+    for _,v := range s {
+        i := int(v-'a')
+        chars[i]++
+    }
+    for _,v := range t {
+        i:=int(v-'a')
+        chars[i]--
+    }
+
+    for _,v := range chars {
+        if v !=0 {
+            return false
+        }
+    }
+    
+    return true
+}
